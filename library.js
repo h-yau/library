42212cebabddbbcafe21d1aa6ee1fb710dd2d359
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-08-23 17:06:50 -0700

    Add Boostrap and basic form functionality

--- library.js
+++ library.js
@@ -18,30 +18,35 @@ function addBookToDisplay(newBook) {
     const booksContainer = document.querySelector('.books-container');
 
     const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
+    bookCard.classList.add("book-card", "card");
+
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('btn-close', 'delete');
+    bookCard.appendChild(closeButton);
 
     const propertyClasses = {
-        author: "authors",
-        title: "titles",
-        pages: "pages",
-        read: "readButtons",
+        author: "h5 fst-italic card-header",
+        title: "display-4 card-title",
+        pages: "pages card-text",
+        read: "readButtons btn",
     }
 
     for (key in newBook) {
         if (key !== 'read') {
             const element = document.createElement('div');
-            element.classList.add(propertyClasses[key]);
+            element.classList.add(...propertyClasses[key].split(' '));
             const formattedKey = `${key[0].toUpperCase()}${key.slice(1)}`;
             element.textContent = `${formattedKey}: ${newBook[key]}`;
             bookCard.appendChild(element);
         } else {
             const readButton = document.createElement('button');
-            readButton.classList.add(propertyClasses[key]);
+            readButton.classList.add(...propertyClasses[key].split(' '));
             if (newBook[key] === true) {
                 readButton.textContent = 'Read';
-                readButton.classList.add('hasRead');
+                readButton.classList.add('btn-success');
             } else {
                 readButton.textContent = 'Unread';
+                readButton.classList.add('btn-warning');
             }
             bookCard.appendChild(readButton);
         } 
@@ -55,7 +60,8 @@ function toggleReadButton(e) {
     e.preventDefault();
 
     const targetButton = e.target;
-    targetButton.classList.toggle('hasRead');
+    targetButton.classList.toggle('btn-success');
+    targetButton.classList.toggle('btn-warning')
 
     if (targetButton.textContent === 'Read') {
         targetButton.textContent = 'Unread';
@@ -65,13 +71,28 @@ function toggleReadButton(e) {
 
 }
 
+function expandQuestionnaire(e) {
+    e.preventDefault();
+
+    e.target.classList.add('open');
+}
+
 // Fake books
+addBookToLibrary("24", "234", 245, false);
+addBookToLibrary("256", "281", 35, false);
+addBookToLibrary("123", "234", 123, true);
+addBookToLibrary("24", "234", 245, false);
+addBookToLibrary("256", "281", 35, false);
+addBookToLibrary("123", "234", 123, true);
+addBookToLibrary("24", "234", 245, false);
+addBookToLibrary("256", "281", 35, false);
 addBookToLibrary("123", "234", 123, true);
 addBookToLibrary("24", "234", 245, false);
 addBookToLibrary("256", "281", 35, false);
 
-// click-event listener
+// click-event listener for readButtons
 const readButtons = document.querySelectorAll('.readButtons');
 readButtons.forEach(readButton => {
     readButton.addEventListener('click', toggleReadButton);
 });
+
